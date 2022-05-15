2020de65310bec9e8ba9097ba7907494f69b2e2e
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-14 22:31:25 -0300

    feat: Adicionar novos metodos

--- src/api/categorias.js
+++ src/api/categorias.js
@@ -9,6 +9,33 @@ const CategoriasAPI = {
     } catch(error) {
       console.error(error);
     }
+  },
+
+  add: async (categoria) => {
+    try {
+      const response = await api.post("/categorias", {...categoria});
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  edit: async (categoria) => {
+    try {
+      const response = await api.put(`/categorias/${categoria.id_categoria}`, {...categoria});
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  delete: async (id_categoria) => {
+    try {
+      const response = await api.delete(`/categorias/${id_categoria}`);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
 }
