b1878cfe904e8dea552aec6b752adcb22f06cbcd
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-14 22:50:43 -0300

    feat: Implementa nova api

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 import CategoriasAPI from "./categorias";
+import ProdutosAPI from "./produtos";
 
 const api = axios.create({
   baseURL: "http://localhost:3001/api"
 });
 
 export {
-  CategoriasAPI
+  CategoriasAPI,
+  ProdutosAPI
 }
+
 export default api;
