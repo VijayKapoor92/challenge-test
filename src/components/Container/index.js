82cc4b63f57d5eba64c64362c67a6dfad3220cea
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-15 15:58:52 -0300

    fix: Arruma bug

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -7,25 +7,17 @@ const Container = styled.div `
   margin-right: auto;
   margin-left: auto;
 
-  @media (min-width: 576px) {
-    .container {
-      max-width: 540px;
-    }
+  @media(min-width: 576px) {
+    max-width: 540px;
   }
-  @media (min-width: 768px) {
-    .container {
-      max-width: 720px;
-    }
+  @media(min-width: 768px) {
+    max-width: 720px;
   }
-  @media (min-width: 992px) {
-    .container {
-      max-width: 960px;
-    }
+  @media(min-width: 992px) {
+    max-width: 960px;
   }
-  @media (min-width: 1200px) {
-    .container {
-      max-width: 1140px;
-    }
+  @media(min-width: 1200px) {
+    max-width: 1140px;
   }
 `;
 
