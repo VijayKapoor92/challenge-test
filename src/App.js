a7d35bbb06f1dea421b876f44d01ba2fad0d1156
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-14 21:50:13 -0300

    feat: Carrega categorias ao comecar o app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import React, { useEffect } from "react";
+
+import { CategoriasAPI } from "./api";
+
 function App() {
+
+  useEffect(() => {
+    CategoriasAPI.getAll()
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div></div>
   );
