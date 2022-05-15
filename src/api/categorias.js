c9d4db1dfd965d61b7bc9a56b523cafe85bc03a8
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-14 21:52:27 -0300

    refactor: Remove debug

--- src/api/categorias.js
+++ src/api/categorias.js
@@ -3,11 +3,8 @@ import api from "./";
 const CategoriasAPI = {
 
   getAll: async () => {
-    console.log("Entrou");
     try {
-      console.log("Dispatch");
       const response = await api.get("/categorias");
-      console.log(response);
       return response;
     } catch(error) {
       console.error(error);
