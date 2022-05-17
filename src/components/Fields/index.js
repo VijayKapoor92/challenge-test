6981a42a9221ea51b22653522aeac4e61f3576b9
VijayKapoor92 <vijayk_1@hotmail.com>
2022-05-17 11:45:29 -0300

    styles: Implementa novos estilos

--- src/components/Fields/index.js
+++ src/components/Fields/index.js
@@ -2,21 +2,30 @@ import styled from "styled-components";
 
 export const Input = styled.input `
   width: 100%;
-  border: 1px solid rgba(0, 0, 0, .24);
+  border: 1px solid #757575;
   border-radius: .3rem;
   padding: .4rem;
   font-size: .84375rem;
   outline: 0;
 
+  transition: border-color 100ms linear;
+
   &:focus {
-    border-color: rgba(0, 0, 0, .34);
+    border-color: #000000;
   }
 `;
 
 export const Select = styled.select `
   width: 100%;
-  border: 1px solid rgba(0, 0, 0, .24);
+  border: 1px solid #757575;
   border-radius: .2rem;
   padding: .4rem;
   font-size: .84375rem;
+  outline: 0;
+
+  transition: border-color 100ms linear;
+
+  &:focus {
+    border-color: #000000;
+  }
 `;
